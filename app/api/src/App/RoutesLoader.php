0d4ac081f6d5af886d391e895234a033c41db92e
hyanemoussassa <hyanemoussassa1@gmail.com>
2015-12-16 20:20:32 +0000

    Fixed issues with Mailservice and made some changes to swagger
    1. Fixed issues with MailService
    2. Changed apache host file DocumentRoot and Servername
    3. Moved swagger-ui from the html folder into the app folder.
    4. Deleted swagger_petstore as this is not needed
    5. Added swagger entries for all the API endpoints

--- app/api/src/App/RoutesLoader.php
+++ app/api/src/App/RoutesLoader.php
@@ -28,9 +28,63 @@ class RoutesLoader
 
 
     }
+
+    /**
+     *  @SWG\Definition(
+     *    @SWG\Xml(name="TokenDefault"),
+     *    definition = "TokenDefault",
+     * 		required={"success", "token"},
+     * 		@SWG\Property(property="success", type="boolean"),
+     * 		@SWG\Property(property="token", type="string", description="Json Web Token"),
+     *    @SWG\Property(property="stacktrace", type="string", description="stack trace")
+     * 	),
+     *  @SWG\Definition(
+     *    @SWG\Xml(name="ErrorDefault"),
+     *    definition = "ErrorDefault",
+     * 		required={"statusCode", "message"},
+     * 		@SWG\Property(property="statusCode", type="boolean"),
+     * 		@SWG\Property(property="message", type="string", description="Message detailing error")
+     * 	),
+     *  @SWG\Definition(
+     *    @SWG\Xml(name="User"),
+     *    definition = "User",
+     * 		required={"email", "fullname","password"},
+     * 		@SWG\Property(
+     *      property="user",
+     *      type="object",
+     * 		  required={"email", "fullname","password"},
+     * 		  @SWG\Property(property="email", type="string"),
+     *      @SWG\Property(property="fullname", type="string"),
+     * 		  @SWG\Property(property="password", type="string", description="Password")
+     *    )
+     * 	)
+     */
     public function bindRoutesToControllers()
     {
         $api = $this->app["controllers_factory"];
+
+        /**
+         *  @SWG\Get(
+         *    path="/user/{id}",
+         *    tags={"user"},
+         *    operationId="getuser",
+         *    summary="Gets all the details of a specified user",
+         *    description="This service is used to retrieve all the details about the user that we hold in the database.",
+         *    consumes={"application/json","application/xml"},
+         *    produces={"application/json","application/xml"},
+         *    @SWG\Parameter(
+         *      name="id",
+         *      in="path",
+         *      required=true,
+         *      type="string",
+         *      description="UUID"
+         *    ),
+         *    @SWG\Response(
+         *      status=200,
+         *      description="success"
+         *    )
+         *  )
+         */
         $api->get('/user/{id}', "users.controller:get");
 
         /**
@@ -40,8 +94,8 @@ class RoutesLoader
          *    operationId="userSignUp",
          *    summary="Registers the user to the pardna site",
          *    description="This service is used to create a Pardna Account for a user. User details are stored in DB and the user is sent a mail  to confirm account using Mailchimp mailing list",
-         *    consumes={"application/xml", "application/json"},
-         *    produces={"application/xml", "application/json"},
+         *    consumes={"application/json", "application/xml"},
+         *    produces={"application/json", "application/xml"},
          *    @SWG\Parameter(
          *      name="User",
          *      in="body",
@@ -60,7 +114,7 @@ class RoutesLoader
          *    ),
          *    security={
          *         {
-         *             "petstore_auth": {"write:pets", "read:pets"}
+         *             "pardna_auth": {"write:pardna", "read:pardna"}
          *         }
          *     }
          *  )
@@ -68,19 +122,26 @@ class RoutesLoader
         $api->post('/signup', "users.controller:signup");
 
         /**
+         *  @SWG\Definition(
+         *      @SWG\Xml(name="LoginUser"),
+         *      definition = "LoginUser",
+         * 			required={"username", "password"},
+         * 			@SWG\Property(property="username", type="string", description="Username"),
+         * 			@SWG\Property(property="password", type="string", description="Password")
+         * 	),
          *  @SWG\Post(
          *    path="/login",
          *    tags={"user"},
          *    operationId="userLogIn",
          *    summary="Enables the user to login to the pardna site",
          *    description="This service is used to login to the pardna site",
-         *    consumes={"application/xml", "application/json"},
-         *    produces={"application/xml", "application/json"},
+         *    consumes={"application/json", "application/xml"},
+         *    produces={"application/json", "application/xml"},
          *    @SWG\Parameter(
          *      name="User",
          *      in="body",
          *      required = true,
-         *      description="User object that needs to be registered to Pardna",
+         *      description="User object that needs to be logged in to Pardna",
          *      @SWG\Schema(ref="#/definitions/LoginUser")
          *    ),
          *    @SWG\Response(
@@ -100,20 +161,118 @@ class RoutesLoader
          *    ),
          *    security={
          *         {
-         *             "petstore_auth": {"write:pets", "read:pets"}
+         *             "pardna_auth": {"write:pardna", "read:pardna"}
          *         }
          *     }
          *  )
          */
         $api->post('/login', "users.controller:login");
+
         $api->get('/login', "users.controller:login");
+
+        /**
+         *  @SWG\Post(
+         *    path="/refresh-token",
+         *    tags={"user"},
+         *    operationId="refreshToken",
+         *    summary="Refreshes the login token",
+         *    description="Refreshes the login token. No request object required",
+         *    consumes={"application/json", "application/xml"},
+         *    produces={"application/json", "application/xml"},
+         *    security={
+       *         {
+       *             "pardna_auth": {"write:pardna", "read:pardna"}
+       *         }
+         *    }
+         *  )
+         */
         $api->post('/refresh-token', "users.controller:refreshToken");
 
         $api->post('/signin', "users.controller:signin");
+
+        /**
+         *  @SWG\Definition(
+         *        @SWG\Xml(name="ChangePasswordUser"),
+         *        definition = "ChangePasswordUser",
+         * 			  @SWG\Property(
+         *          property="user",
+         *          type="object",
+         * 			    required={"email", "currentPassword","newPassword"},
+         * 			    @SWG\Property(property="email", type="string"),
+         * 			    @SWG\Property(property="currentPassword", type="string", description="currentPassword"),
+         * 			    @SWG\Property(property="newPassword", type="string", description="newPassword")
+         *        )
+         * 	),
+         *  @SWG\Post(
+         *    path="/user/change-password",
+         *    tags={"user"},
+         *    operationId="changeUserPassword",
+         *    summary="Change user password",
+         *    description="This service is used to change a specified user password",
+         *    consumes={"application/json", "application/xml"},
+         *    produces={"application/json", "application/xml"},
+         *    @SWG\Parameter(
+         *      name="User",
+         *      in="body",
+         *      required = true,
+         *      description="User object, whose password needs to be changed" ,
+         *      @SWG\Schema(ref="#/definitions/ChangePasswordUser")
+         *    ),
+         *    @SWG\Response(
+         *      response="401",
+         *      description="Invalid username or password",
+         *      @SWG\Schema(ref="#/definitions/ErrorDefault")
+         *    ),
+         *    @SWG\Response(
+         *      response="200",
+         *      description="User is successfuly registered",
+         *      @SWG\Schema(ref="#/definitions/TokenDefault")
+         *    )
+         *  )
+         */
         $api->post('/user/change-password', "users.controller:changePassword");
         $api->put('/user/{id}', "users.controller:update");
         $api->delete('/user/{id}', "users.controller:delete");
 
+        /**
+         *  @SWG\Definition(
+         *      @SWG\Xml(name="MobilePhoneRequest"),
+         *      definition = "MobilePhoneRequest",
+         * 			required={"mobile"},
+         * 			@SWG\Property(property="mobile", type="string", description="mobile")
+         * 	),
+         *  @SWG\Post(
+         *    path="/user/send-code",
+         *    tags={"user"},
+         *    operationId="sendCodeForMobilePhoneConfirmation",
+         *    summary="Sends code to a user mobile phone",
+         *    description="This service is used to send the user a code which will be used to confirm mobile phone registration",
+         *    consumes={"application/json", "application/xml"},
+         *    produces={"application/json", "application/xml"},
+         *    @SWG\Parameter(
+         *      name="VerifyUserPhoneNumberRequest",
+         *      in="body",
+         *      required = true,
+         *      description="Verify User PhoneNumber",
+         *      @SWG\Schema(ref="#/definitions/MobilePhoneRequest")
+         *    ),
+         *    @SWG\Response(
+         *      response="401",
+         *      description="Invalid username or password",
+         *      @SWG\Schema(ref="#/definitions/ErrorDefault")
+         *    ),
+         *    @SWG\Response(
+         *      response="200",
+         *      description="User is successfuly registered",
+         *      @SWG\Schema(ref="#/definitions/TokenDefault")
+         *    ),
+         *    security={
+         *         {
+         *             "pardna_auth": {"write:pardna", "read:pardna"}
+         *         }
+         *    }
+         *  )
+         */
         $api->post('/user/send-code', "users.controller:sendCode");
 
         $api->post('/user/verify', "users.controller:verify");
--- app/api/src/swagger/security.php
+++ app/api/src/swagger/security.php
@@ -1,21 +1,13 @@
 <?php
-/**
- * @SWG\SecurityScheme(
- *   securityDefinition="api_key",
- *   type="apiKey",
- *   in="header",
- *   name="api_key"
- * )
- */
-/**
- * @SWG\SecurityScheme(
- *   securityDefinition="petstore_auth",
- *   type="oauth2",
- *   authorizationUrl="http://petstore.swagger.io/api/oauth/dialog",
- *   flow="implicit",
- *   scopes={
- *     "read:pets": "read your pets",
- *     "write:pets": "modify pets in your account"
- *   }
- * )
- */
+ /**
+  * @SWG\SecurityScheme(
+  *   securityDefinition="pardna_auth",
+  *   type="oauth2",
+  *   authorizationUrl="http://192.168.33.99/app/frontend/dist/#/login",
+  *   flow="implicit",
+  *   scopes={
+  *     "read:pardna": "Allowed to see from Pardna site",
+  *     "write:pardna": "Allowed to modify from Pardna site"
+  *   }
+  * )
+  */
