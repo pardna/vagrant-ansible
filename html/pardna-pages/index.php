f376a8b74303471cb066d3bd772df51112b7b680
phenriques <phenriques@me.com>
2016-02-10 23:30:08 +0000

    New html homepage for pre-launch Pardna.com

--- html/pardna-pages/index.php
+++ html/pardna-pages/index.php
@@ -5,7 +5,7 @@
 <!-- Basic Page Needs
 ------------------------------------------------------------------------->
   <meta charset="utf-8">
-  <title>Pardna.com</title>
+  <title>Pardna.com - Group Saving And Lending</title>
   <meta name="description" content="">
   <meta name="author" content="">
 
@@ -22,6 +22,16 @@
 ------------------------------------------------------------------------->
     <link rel="icon" type="image/png" href="img/favicon.png">
 
+    
+
+<!-- Facebook Open Graph
+------------------------------------------------------------------------->    
+<meta property="og:title" content="Register for early access" />
+<meta property="og:type" content="company" />
+<meta property="og:site_name" content="Register for early access" />
+<meta property="og:url" content="www.pardnermoney.com/pardna-pages/" />
+<meta property="og:image" content="https://launchrock-assets.s3.amazonaws.com/facebook-files/0WUJB8Z9_1374750449395.jpg?_=0" />    
+    
 </head>
 <body id="home">
 
@@ -31,16 +41,23 @@
   	
 <!--- Header
 ------------------------------------------------------------------------->
-      <?php include 'inc/_header.php';?>
+	<div class="row header">
+    	<div class="four columns logo"><a href="index.php"></a></div>
+    </div>
 
 <!-- Hero
 ------------------------------------------------------------------------->
 <div class="hero">
-	<div class="hero__strapline eight columns">
-	    	<h1>Save up the easy way with close friends and family. Safe, secure and hassle free.</h1>
-        <h2>Finance your dreams responsibly with a Pardna!</h2>
-    	<a href="register.php" class="button button-primary">Get Started!</a>
-    </div>
+    <div class="hero_darken">
+        <div class="hero__strapline eight columns">
+                <h1>Finance your dreams with close friends and family.</h1>
+            <h2>Pardna.com - the group saving and lending platform</h2>
+            <!-- Begin LaunchRock Widget -->
+            <div id="lr-widget" rel="0WUJB8Z9"></div>
+            <script type="text/javascript" src="//ignition.launchrock.com/ignition-current.min.js"></script>
+            <!-- End LaunchRock Widget -->
+        </div>
+    </div>    
 </div>
 
 <!-- Value Proposition
@@ -48,52 +65,46 @@
 <div class="row valueprop">
 	<div class="four columns valueprop__group">
     	<span class="icon"></span>
-    	<h3>Group Saving</h3>
-        Vivamus nec aliquet dolor. Quisque ut felis magna. Suspendisse convallis vestibulum pellentesque. 
+    	<h3>Group Saving &amp; Lending</h3>
+        Achieve your financial goals with the people you love and trust the most.
     </div>
 	<div class="four columns valueprop__secure">
     	<span class="icon"></span>
     	<h3>Secure & Safe</h3>
-        Fusce convallis laoreet volutpat. Maecenas ornare lacus sit amet lacus aliquet, at ultrices dui tempor. 
+        We provide a secure platform to conduct your group saving and lending activities. 
     </div>
 	<div class="four columns valueprop__rates">
     	<span class="icon"></span>
-    	<h3>Low Rate</h3>
-        Curabitur ex libero, accumsan in urna in, scelerisque sagittis eros. Integer feugiat.
+    	<h3>Low Fees</h3>
+        Low and transparent fees so you keep more of your money for you.
     </div>
 </div>
 
-<!-- Carousel
-------------------------------------------------------------------------->
-    <div class="row carousel">
-	<h1>How a Pardna Works</h1>
-        <ul rn-carousel  rn-carousel-controls class="image">
-          <li>slide #1</li>
-          <li>slide #2</li>
-          <li>slide #3</li>
-          <li>slide #4</li>
-          <li>slide #5</li>
-          <li>slide #6</li>
-        </ul>
-    </div>
     
 <!-- Panel
 ------------------------------------------------------------------------->      
 <div class="row panel">    
     <div class="row twelve columns">
-        <h1>The Trusted Way To Save Money The World Over</h1>
+        <h1>The Trusted Way To Save and Lend Money The World Over</h1>
     </div>
     <div class="row">
         <div class="six columns world"></div>
         <div class="six columns">
-        Donec pretium pharetra dapibus. Praesent vitae eros mauris. In viverra diam nec sapien mollis efficitur. Etiam accumsan leo ac elit tincidunt rutrum. Ut condimentum est dolor, a volutpat purus ultrices id. Pellentesque eu purus ut ex ornare venenatis eu eu augue. Quisque sed augue non mi pulvinar faucibus in ut purus. Donec molestie viverra hendrerit.
+            Pardna.com operates the rotating saving and credit method of money management. This method is one of the oldest financial tools in history and is known by many names all over the world: <i>Susu</i> or <i>Likelemba</i> in Western Africa, <i>Committee</i> or <i>Hui</i> in Asia, <i>Pardna</i> in the Caribbean. The basic operation is the same: a group of individuals come together to make repeated contributions and withdrawals to and from a common fund. Pardna.com - the trusted way to save and lend money the world over.
         </div>
     </div>
 </div>
 
 <!-- Footer
 ------------------------------------------------------------------------->
-<?php include 'inc/_footer.php';?>
+<div class="row footer">
+    <div class="six columns footer__social" style="text-align:center">
+        <span class="footer__social--twitter"><a href="http://www.twitter.com/PardnaMoney">@PardnaMoney</a></span>
+    </div>
+    <div class="six columns footer__social" style="text-align:center">
+        <span class="footer__social--email"><a href="#">contact@pardna.com</a></span>
+    </div>    
+</div>
 
 <!-- End Container
 ------------------------------------------------------------------------->
