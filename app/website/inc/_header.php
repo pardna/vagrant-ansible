7f896840df17766ca809c471c01a752fe44b3c8e
Pa-Essa Jabang <pjabang@gmail.com>
2017-02-04 12:31:21 +0000

    header link

--- app/website/inc/_header.php
+++ app/website/inc/_header.php
@@ -1,8 +1,8 @@
   <nav class="purple" role="navigation">
     <div class="nav-wrapper container">
-      <a id="logo-container" href="http://192.168.33.99/app/website" class="brand-logo"></a>
+      <a id="logo-container" href="/blog" class="brand-logo"></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="http://192.168.33.99/app/website/blog/">Blog</a></li>
+        <li><a href="/blog">Blog</a></li>
       </ul>
     </div>
   </nav>
